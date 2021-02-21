ec6dae203488b3ba3678121eeb909f7df30cd6e7
Oscar Gichana <gichanaoscar3@gmail.com>
2021-02-21 12:26:29 +0300

    sending request to get sources.

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ class Source:
     Source class to define source Objects
     '''
 
-    def __init__(self,id,name,description,url,urlToImage):
+    def __init__(self,id,name,description,url):
         self.id =id
         self.name = name
         self.url = url
