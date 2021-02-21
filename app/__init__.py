b83790a1b01a2e315700066d2a82e12aba47afcd
Oscar Gichana <gichanaoscar3@gmail.com>
2021-02-21 12:39:49 +0300

    initializing bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,13 @@ def create_app(config_name):
 # Creating the app configurations
     app.config.from_object(config_options[config_name])
 
+# Initializing flask extensions
+    bootstrap.init_app(app)
+
+# Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
 # setting config
     from .request import configure_request
     configure_request(app)
