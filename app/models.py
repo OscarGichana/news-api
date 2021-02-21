f278174885ff940af01c02364338cf7de56f202c
Oscar Gichana <gichanaoscar3@gmail.com>
2021-02-21 12:31:25 +0300

    sending request to get article.

--- app/models.py
+++ app/models.py
@@ -7,20 +7,16 @@ class Source:
         self.id =id
         self.name = name
         self.url = url
-        self.urlToImage = str(urlToImage)
 
 
 class Article:
 
-    def __init__(self,id,name,urlToImage,author,title,url,description,publishedAt,content):
-        self.id = id
-        self.name = name
+    def __init__(self,urlToImage,author,title,url,description,publishedAt):
         self.urlToImage = urlToImage
         self.author = author
         self.title = title
         self.url = url
         self.description = description
         self.publishedAt = publishedAt
-        self.content = content
 
 
