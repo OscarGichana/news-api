8143a4abecb098044c4d0d334ab7df0fd1f7285d
Oscar Gichana <gichanaoscar3@gmail.com>
2021-02-21 12:39:00 +0300

    creating and setting app configurations.

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+# Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+# setting config
+    from .request import configure_request
+    configure_request(app)
+
+
+    return app
