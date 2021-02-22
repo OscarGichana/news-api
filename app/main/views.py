3158f8c24d954902a9d529fbf7a8d867562182db
Oscar Gichana <gichanaoscar3@gmail.com>
2021-02-22 19:16:36 +0300

    add index page and macros.

--- app/main/views.py
+++ app/main/views.py
@@ -38,3 +38,13 @@ def general(category):
     return render_template('general.html', general = news_categories_articles)
 
 
+@main.route('/categories/<category>')
+def business(category):
+    '''
+    view root page function that returns the categories page and its data
+    '''
+    news_categories_articles = get_category(category)
+
+    return render_template('business.html', business = news_categories_articles)
+
+
