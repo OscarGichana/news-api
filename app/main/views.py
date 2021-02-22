63b597b69d39ad7bdec0d7bf0bfa98a9eb84178f
Oscar Gichana <gichanaoscar3@gmail.com>
2021-02-22 19:20:08 +0300

    add health and entertainment categories

--- app/main/views.py
+++ app/main/views.py
@@ -48,3 +48,21 @@ def business(category):
     return render_template('business.html', business = news_categories_articles)
 
 
+@main.route('/categories/<category>')
+def entertainment(category):
+    '''
+    view root page function that returns the categories page and its data
+    '''
+    news_categories_articles = get_category(category)
+
+    return render_template('entertainment.html', entertainment=news_categories_articles)
+
+
+@main.route('/categories/<category>')
+def health(category):
+    '''
+    view root page function that returns the categories page and its data
+    '''
+    news_categories_articles = get_category(category)
+
+    return render_template('health.html', health = news_categories_articles)
